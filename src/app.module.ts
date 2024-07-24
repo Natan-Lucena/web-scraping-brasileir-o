f77bdeb2b5051a7a87eb733d54bd8dac0bf7d66d
Natan Lucena <zerogamerptbr@gmail.com>
2024-07-24 17:19:48 -0300

    :construction: build: add bull instances to inject in the modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { InfraModule } from './infra/infra.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaService } from './prisma/prisma.service';
+import 'dotenv/config';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [InfraModule, ScheduleModule.forRoot()],
+  imports: [
+    InfraModule,
+    ScheduleModule.forRoot(),
+    BullModule.forRoot({
+      url: process.env.REDIS_URL,
+      defaultJobOptions: {
+        removeOnComplete: true,
+        removeOnFail: 100,
+        attempts: 3,
+      },
+    }),
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
