3e94609cf894c631d26d07738f99dcf32685d662
Natan Lucena <zerogamerptbr@gmail.com>
2024-07-28 12:41:07 -0300

    :sparkles: feat: add new leagues URLs

--- src/infra/services/save-team-data/save-team-data.service.ts
+++ src/infra/services/save-team-data/save-team-data.service.ts
@@ -24,15 +24,21 @@ export class SaveTeamDataService {
       'https://www.google.com/search?q=tabela+do+brasileirao&oq=tabela+do+brasileirao&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQLhhA0gEINDc3MmowajGoAgCwAgA&sourceid=chrome&ie=UTF-8',
       'https://www.google.com/search?q=tabela+premier+league&sca_esv=2a9720fd994fa302&sxsrf=ADLYWIIMJeHR6xlUARN1JHZ7tI9oWN3Rog%3A1721851897430&ei=-V-hZsHxGf7R1sQPo5ySCA&ved=0ahUKEwiB0s6VvsCHAxX-qJUCHSOOBAEQ4dUDCA8&uact=5&oq=tabela+premier+league&gs_lp=Egxnd3Mtd2l6LXNlcnAiFXRhYmVsYSBwcmVtaWVyIGxlYWd1ZTIKECMYgAQYJxiKBTIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjoHFDiBFjKG3ACeAGQAQCYAasBoAHoEqoBBDAuMTW4AQPIAQD4AQGYAhGgApgTwgIKEAAYsAMY1gQYR8ICDRAAGIAEGLADGEMYigXCAg4QABiwAxjkAhjWBNgBAcICExAuGIAEGLADGEMYyAMYigXYAQLCAgwQIxiABBgTGCcYigXCAhAQABiABBixAxhDGIMBGIoFwgIIEAAYgAQYsQPCAgoQABiABBhDGIoFwgINEAAYgAQYsQMYQxiKBcICDhAAGIAEGLEDGIMBGIoFwgIHECMYsQIYJ8ICBxAAGIAEGAqYAwCIBgGQBhO6BgYIARABGAm6BgYIAhABGAiSBwQyLjE1oAecbg&sclient=gws-wiz-serp',
       'https://www.google.com/search?q=tabela+campeonato+italiano&sca_esv=2a9720fd994fa302&sxsrf=ADLYWILgHmmw4gT41T2FEhTt_Loh7WyooQ%3A1721851911871&ei=B2ChZo_sNMLX1sQP6Ziu4A0&ved=0ahUKEwjPi8CcvsCHAxXCq5UCHWmMC9wQ4dUDCA8&uact=5&oq=tabela+campeonato+italiano&gs_lp=Egxnd3Mtd2l6LXNlcnAiGnRhYmVsYSBjYW1wZW9uYXRvIGl0YWxpYW5vMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjkLlDdB1i8LXADeAGQAQGYAbkCoAGeIaoBCDAuMTkuMy4xuAEDyAEA-AEBmAIZoAK5H8ICChAAGLADGNYEGEfCAg0QABiABBiwAxhDGIoFwgIOEAAYsAMY5AIY1gTYAQHCAhMQLhiABBiwAxhDGMgDGIoF2AECwgIKECMYgAQYJxiKBcICDBAjGIAEGBMYJxiKBcICCxAAGIAEGLEDGIMBwgIIEAAYgAQYsQPCAgcQABiABBgKwgIHEAAYgAQYDZgDAIgGAZAGE7oGBggBEAEYCboGBggCEAEYCJIHBjMuMTkuM6AHsJcB&sclient=gws-wiz-serp',
+      'https://www.google.com/search?client=opera-gx&q=tabela+MLS&sourceid=opera&ie=UTF-8&oe=UTF-8',
+      'https://www.google.com/search?q=tabela+campeonato+saudita&client=opera-gx&hs=ktU&sca_esv=c839f9702c677c11&sxsrf=ADLYWILuwtmHSIg9dlM8csZtcpm0ju1zCQ%3A1722180024052&ei=uGGmZujuArTr1AGUyMq4DA&ved=0ahUKEwjoqsrEhMqHAxW0NTUKHRSkEscQ4dUDCBA&uact=5&oq=tabela+campeonato+saudita&gs_lp=Egxnd3Mtd2l6LXNlcnAiGXRhYmVsYSBjYW1wZW9uYXRvIHNhdWRpdGEyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyCBAAGIAEGMsBMggQABiiBBiJBTIIEAAYogQYiQVI8jdQ5gtYsDdwBngAkAEAmAHtAaAB4x-qAQYwLjIzLjG4AQPIAQD4AQGYAh6gAqYgqAISwgILEAAYgAQYsQMYgwHCAgoQABiABBhDGIoFwgIIEAAYgAQYsQPCAgoQIxiABBgnGIoFwgIOEAAYgAQYsQMYgwEYigXCAgcQIxgnGOoCwgIWEC4YgAQYQxi0AhjIAxiKBRjqAtgBAcICChAuGIAEGEMYigXCAgUQLhiABMICBRAhGKABmAMGugYGCAEQARgIkgcGNi4yMy4xoAfFxQE&sclient=gws-wiz-serp',
+      'https://www.google.com/search?q=tabela+serie+b&client=opera-gx&hs=PuU&sca_esv=c839f9702c677c11&sxsrf=ADLYWIKPpvZttkVFG9lnmqE9qHlC2L3fmQ%3A1722180065524&ei=4WGmZt_OH_7Y1sQPi86WsAY&ved=0ahUKEwifw63YhMqHAxV-rJUCHQunBWYQ4dUDCBA&uact=5&oq=tabela+serie+b&gs_lp=Egxnd3Mtd2l6LXNlcnAiDnRhYmVsYSBzZXJpZSBiMgoQIxiABBgnGIoFMggQABiABBixAzIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAESJk4UIMeWIU2cAF4AZABAJgBrQGgAa0LqgEDMC45uAEDyAEA-AEBmAIKoALPC8ICChAAGLADGNYEGEfCAg0QABiABBiwAxhDGIoFwgITEC4YgAQYsAMYQxjIAxiKBdgBAcICCxAAGIAEGLEDGIMBwgIKEAAYgAQYQxiKBZgDAIgGAZAGFLoGBggBEAEYCJIHAzEuOaAH800&sclient=gws-wiz-serp',
+      'https://www.google.com/search?q=tabela+segunda+divisão+inglesa&client=opera-gx&sca_esv=c839f9702c677c11&sxsrf=ADLYWIIt-n9kzEifVNlNPFJohA-QJPKTfw%3A1722180462400&ei=bmOmZuSKGPvd1sQPrvO0kAs&oq=tabela+segunda+di&gs_lp=Egxnd3Mtd2l6LXNlcnAiEXRhYmVsYSBzZWd1bmRhIGRpKgIIATIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARI_StQsglYkyNwAXgBkAEAmAGtAaABrRCqAQQwLjEzuAEDyAEA-AEBmAIOoALREMICChAAGLADGNYEGEfCAg0QABiABBiwAxhDGIoFwgIKECMYgAQYJxiKBcICChAAGIAEGEMYigXCAgsQABiABBixAxiDAcICCBAAGIAEGLEDmAMAiAYBkAYKkgcEMS4xM6AH91k&sclient=gws-wiz-serp',
+      'https://www.google.com/search?q=tabela+campeonato+frances&client=opera-gx&sca_esv=c839f9702c677c11&sxsrf=ADLYWII50sqZhC4I4TI4F6HOCBH5fbwtRA%3A1722180471785&ei=d2OmZr3UL47L1sQPxqu7sA8&ved=0ahUKEwj964mahsqHAxWOpZUCHcbVDvYQ4dUDCBA&uact=5&oq=tabela+campeonato+frances&gs_lp=Egxnd3Mtd2l6LXNlcnAiGXRhYmVsYSBjYW1wZW9uYXRvIGZyYW5jZXMyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAESNAlUM8HWMAkcAF4AZABAJgBtgGgAcgZqgEEMC4yMLgBA8gBAPgBAZgCFaAC-xnCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBcICChAjGIAEGCcYigXCAgsQABiABBixAxiDAcICCBAAGIAEGLEDwgIFEC4YgASYAwCIBgGQBgqSBwQxLjIwoAfwoQE&sclient=gws-wiz-serp',
+      'https://www.google.com/search?q=tabela+laliga&client=opera-gx&sca_esv=c839f9702c677c11&sxsrf=ADLYWIKegraTt4MllE6cFUWzrS5WRASkDA%3A1722180519140&ei=p2OmZoehCMfN1sQP56fmqAk&ved=0ahUKEwjHkNSwhsqHAxXHppUCHeeTGZUQ4dUDCBA&uact=5&oq=tabela+laliga&gs_lp=Egxnd3Mtd2l6LXNlcnAiDXRhYmVsYSBsYWxpZ2EyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgcQABiABBgKMgUQABiABDIFEAAYgAQyBxAAGIAEGAoyBRAAGIAEMgcQABiABBgKSNaJAVCEEFjghwFwAngBkAEAmAGcAqABmReqAQYwLjE3LjG4AQPIAQD4AQGYAhSgAtQXwgIKEAAYsAMY1gQYR8ICDRAAGIAEGLADGEMYigXCAhMQLhiABBiwAxhDGMgDGIoF2AEBwgIKECMYgAQYJxiKBcICCxAAGIAEGLEDGIMBwgIKEAAYgAQYQxiKBcICCBAAGIAEGLEDmAMAiAYBkAYMugYECAEYCJIHBjIuMTcuMaAHv5oB&sclient=gws-wiz-serp',
+      'https://www.google.com/search?q=tabela+portugal&client=opera-gx&sca_esv=c839f9702c677c11&sxsrf=ADLYWIKaKJJUSKFUoPncuxkIBS2dDL38Nw%3A1722180605588&ei=_WOmZoXHI7PY1sQPmPXB0A8&ved=0ahUKEwjFufDZhsqHAxUzrJUCHZh6EPoQ4dUDCBA&uact=5&oq=tabela+portugal&gs_lp=Egxnd3Mtd2l6LXNlcnAiD3RhYmVsYSBwb3J0dWdhbDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARIvJoBUL0SWPuYAXABeAGQAQCYAagBoAGRC6oBAzAuObgBA8gBAPgBAZgCCqACtQvCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBcICDhAAGLADGOQCGNYE2AEBwgITEC4YgAQYsAMYQxjIAxiKBdgBAsICChAjGIAEGCcYigXCAgsQABiABBixAxiDAcICChAAGIAEGEMYigXCAggQABiABBixA8ICDhAAGIAEGLEDGIMBGIoFmAMAiAYBkAYTugYGCAEQARgJugYGCAIQARgIkgcDMS45oAf_NQ&sclient=gws-wiz-serp',
+      'https://www.google.com/search?q=tabela+bundesliga&client=opera-gx&sca_esv=c839f9702c677c11&sxsrf=ADLYWILFmmx5sWWCwptvdPr9W7k4Twt_WA%3A1722180749868&ei=jWSmZrHZNM3U1sQPpJXz4AI&ved=0ahUKEwjx09aeh8qHAxVNqpUCHaTKHCwQ4dUDCBA&uact=5&oq=tabela+bundesliga&gs_lp=Egxnd3Mtd2l6LXNlcnAiEXRhYmVsYSBidW5kZXNsaWdhMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEizwQFQuy1Y5MABcAF4AZABAJgBhAOgAaQOqgEHMC42LjIuMbgBA8gBAPgBAZgCCqACyA7CAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBcICExAuGIAEGLADGEMYyAMYigXYAQHCAhkQLhiABBiwAxjRAxhDGMcBGMgDGIoF2AEBwgIKECMYgAQYJxiKBcICCxAAGIAEGLEDGIMBwgIMECMYgAQYExgnGIoFwgIEECMYJ8ICCBAAGIAEGLEDmAMAiAYBkAYUugYGCAEQARgIkgcHMS41LjMuMaAH5Dc&sclient=gws-wiz-serp',
     ];
     urls.map(async (url) => await this.queue.add(JOB_NAME, { url }));
-    console.log('adicionou');
   }
 
   @Process(JOB_NAME)
   async processQueue(job: any) {
     const { url } = job.data;
-    console.log('tratando');
     const browser = await puppeteer.launch({
       headless: true,
       defaultViewport: null,
@@ -58,7 +64,9 @@ export class SaveTeamDataService {
     }, buttonSelector);
 
     if (!buttonClicked) {
-      console.error("Botão 'Mais classificações' não encontrado");
+      console.error(
+        "Botão 'Mais classificações' não encontrado na liga " + url,
+      );
       await browser.close();
       return;
     }
@@ -69,11 +77,15 @@ export class SaveTeamDataService {
 
     let rows;
     let teamsData = await page.evaluate(
-      (tableRowSelector: string, leagueSelector: string) => {
+      async (tableRowSelector: string, leagueSelector: string) => {
         rows = document.querySelectorAll(tableRowSelector);
-        const leagueElement = document.querySelector(leagueSelector) as HTMLElement;
-        const leagueName = leagueElement ? leagueElement.innerText : 'Unknown League';
-        
+        const leagueElement = document.querySelector(
+          leagueSelector,
+        ) as HTMLElement;
+        const leagueName = leagueElement
+          ? leagueElement.innerText
+          : 'Unknown League';
+
         const data = [];
 
         rows.forEach((row) => {
@@ -88,7 +100,18 @@ export class SaveTeamDataService {
           const gcElement = row.querySelector('td:nth-child(10) div');
           const sgElement = row.querySelector('td:nth-child(11) div');
 
-          if (positionElement && nameElement && ptsElement && pjElement && vitElement && eElement && derElement && gmElement && gcElement && sgElement) {
+          if (
+            positionElement &&
+            nameElement &&
+            ptsElement &&
+            pjElement &&
+            vitElement &&
+            eElement &&
+            derElement &&
+            gmElement &&
+            gcElement &&
+            sgElement
+          ) {
             const position = positionElement.innerText;
             const name = nameElement.innerText;
             const points = ptsElement.innerText;
@@ -136,14 +159,17 @@ export class SaveTeamDataService {
       seen.add(key);
       return true;
     });
-    
+
     try {
+      await this.prisma.team.deleteMany({
+        where: { leagueName: teamsData[0].leagueName },
+      });
       await this.prisma.team.createMany({ data: teamsData });
     } catch (e) {
       console.log(e);
     }
 
     await browser.close();
-    console.log('Dados processados com sucesso');
+    console.log('Dados da liga ' + teamsData[0].leagueName + ' com sucesso');
   }
 }
