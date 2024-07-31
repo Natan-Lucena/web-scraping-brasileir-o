c40c2b399a1b01c3055aff25c646532946345662
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-07-31 08:58:25 -0300

    feat: Add filterUniqueTimes utility function

---
+++ src/utils/filterUniqueTimes.ts
@@ -0,0 +1,13 @@
+import { Team } from "@prisma/client";
+
+export const filterUniqueTimes = (teamsData: Team[]): Team[] => {
+    const seen = new Set();
+    return teamsData.filter((team) => {
+        const key = `${team.position}-${team.name}`;
+        if (seen.has(key)) {
+          return false;
+        }
+        seen.add(key);
+        return true;
+    });
+}
