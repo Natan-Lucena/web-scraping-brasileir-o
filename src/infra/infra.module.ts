ded7f30fa5cba5fc5aa0f03694883d473157b912
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-07-31 08:57:15 -0300

    ✨feat: Add filterUniqueTimes function in Module

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SaveTeamDataService } from './services/save-team-data/save-team-data.service';
+import { CheckTeamGameService } from './services/check-team-game/check-team-game.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { BullModule } from '@nestjs/bull';
 import 'dotenv/config';
 
 @Module({
   imports: [BullModule.registerQueue({ name: process.env.QUEUE_NAME })],
-  providers: [SaveTeamDataService, PrismaService],
+  providers: [CheckTeamGameService, PrismaService],
 })
 export class InfraModule {}
