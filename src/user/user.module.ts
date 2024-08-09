47c1f9822576c574c1be1607d5c3de0b69cbd702
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-09 10:49:45 -0300

    🔨refactor: Remove unused code and update UserModule imports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,25 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserCreateController } from './controllers/userCreate.Controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserUpdateService } from './services/userUpdate.service';
-import { UserCreateService } from './services/userCreate.service';
-import { UserDeleteService } from './services/userDelete.service';
 import { ProvidersModule } from 'src/providers/providers.modules';
 import { UserUpadateController } from './controllers/userUpdateController';
-import { UserDeleteController } from './controllers/userDelete.Controller';
 
 @Module({
   imports: [ProvidersModule],
-  providers: [
-    UserUpdateService,
-    UserCreateService,
-    UserDeleteService,
-    PrismaService,
-  ],
-  controllers: [
-    UserCreateController,
-    UserUpadateController,
-    UserDeleteController,
-  ],
+  providers: [UserUpdateService, PrismaService],
+  controllers: [UserUpadateController],
 })
 export class UserModule {}
