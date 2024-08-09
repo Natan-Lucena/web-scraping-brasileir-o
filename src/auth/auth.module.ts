2e2523d709fd9ddc88beb8e0c248eea06b2fc07b
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-09 20:04:37 -0300

    🔨refactor: Update AuthModule imports and add BcryptProviderService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { ProvidersModule } from 'src/providers/providers.modules';
 import { signInUserService } from './services/sign-InService.service';
 import { singInController } from './controllers/sign-In.Controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { EncryptProviderModule } from 'src/encrypt-provider/encrypt-provider.modules';
+import { BcryptProviderService } from 'src/encrypt-provider/bcryptProviderService';
 
 @Module({
-  imports: [ProvidersModule],
-  providers: [signInUserService, PrismaService],
+  imports: [ProvidersModule, EncryptProviderModule],
+  providers: [signInUserService, PrismaService, BcryptProviderService],
   controllers: [singInController],
 })
 export class AuthModule {}
