3b961a1353a2d9eba154d0f5ac017a59e4ba0414
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-10 16:47:11 -0300

    ✨feat: Add async keyword to userUpdate method in userUpdateController

--- src/user/controllers/userUpdateController.ts
+++ src/user/controllers/userUpdateController.ts
@@ -6,7 +6,7 @@ export class UserUpadateController {
   constructor(private userService: UserUpdateService) {}
 
   @Patch('updateRegister/:id')
-  userUpdate(
+  async userUpdate(
     @Param('id') id: string,
     @Body() data: { email?: string; name?: string; phone?: string },
   ) {
