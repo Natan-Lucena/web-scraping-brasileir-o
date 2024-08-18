077a13a572baf8842cc2934cf2f7f1dad083c49f
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-18 20:23:36 -0300

    ♻️ refactor: renaming files

--- src/auth/controllers/sign-Up.Controller.ts
+++ src/auth/controllers/sign-Up.Controller.ts
@@ -1,12 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { signUpUserDto } from '../dtos/createUserDto';
-import { signUpUserService } from '../services/sign-UpService.service';
+import { signUpUserService } from '../services/sign-up/sign-Up.service';
 
 @Controller('auth')
 export class signUpController {
   constructor(private userService: signUpUserService) {}
 
-  @Post('signUp')
+  @Post('sign-up')
   async sigIn(@Body() createUserDto: signUpUserDto) {
     const { email, name, phone, password } = createUserDto;
     return await this.userService.userCreate(email, name, phone, password);
