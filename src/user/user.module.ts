c9b555345f4e1bb30ea2c366fe4587b4d8b42a48
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-08-22 12:10:44 -0300

    ♻️refactor: refactoring user interest structure

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ProvidersModule } from 'src/providers/providers.modules';
-import { UserInterestController } from './controllers/userInterest.controller';
-import { UserInterestService } from './services/userInterest.service';
 import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
+import { CreateUserInterestController } from './controllers/createUserInterest.controller';
+import { ListUserInterestController } from './controllers/listUserInterest.controller';
+import { DeleteUserInterestController } from './controllers/deleteUserInterest.controller';
+import { CreateUserInterestService } from './services/createUserInterest.service';
+import { ListUserInterestService } from './services/listUserInterest.service';
+import { DeleteUserInterestService } from './services/deleteUserInterest.service';
 
 @Module({
   imports: [ProvidersModule],
-  controllers: [UserInterestController],
-  providers: [PrismaService, UserInterestService, JwtStrategy],
+  controllers: [
+    CreateUserInterestController,
+    ListUserInterestController,
+    DeleteUserInterestController,
+  ],
+  providers: [
+    PrismaService,
+    CreateUserInterestService,
+    ListUserInterestService,
+    DeleteUserInterestService,
+    JwtStrategy,
+  ],
 })
 export class UserModule {}
